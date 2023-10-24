6d16cccd2ab22c014040b3d8e04c1f87acaceddc
Nils Matthias <n.matthias@gmx.de>
2023-10-24 10:41:37 +0200

    Calculator to double

---
+++ src/main/java/org/example/Point.java
@@ -0,0 +1,29 @@
+package org.example;
+
+public class Point {
+    double x;
+    double y;
+
+    public double getY() {
+        return y;
+    }
+
+    public double getZ() {
+        return z;
+    }
+
+    double z;
+
+    public double getX() {
+        return x;
+    }
+
+    public Point(double x, double y)
+    {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+
+}
---
+++ src/main/java/org/example/Vector.java
@@ -0,0 +1,55 @@
+package org.example;
+
+public class Vector {
+    double x,y,z;
+
+
+
+    public Vector(double x,double y,double z)
+    {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public void printVector()
+    {
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(z);
+    }
+
+    public Vector add(Vector v)
+    {
+        return new Vector(x+v.x,y+v.y,z+v.z);
+    }
+
+    public Vector subtract(Vector v)
+    {
+        return new Vector(x-v.x,y-v.y,z-v.z);
+    }
+
+    public Vector negation()
+    {
+        return new Vector(-x,-y,-z);
+    }
+
+    public Vector scalarMultiply(double scalar)
+    {
+        return new Vector(x*scalar,y*scalar,z*scalar);
+    }
+
+    public Vector scalarDivide(double scalar)
+    {
+        return new Vector(x/scalar,y/scalar,z/scalar);
+    }
+
+    public boolean equals(Vector v)
+    {
+        return (x == v.x) && (y == v.y) && (z == v.z);
+    }
+    public boolean unequals(Vector v)
+    {
+        return (x != v.x) || (y != v.y) || (z == v.z);
+    }
+}
