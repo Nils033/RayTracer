8239ca303e2c2c1bb384a26c1f9ab53f9535e235
Nils033 <n.matthias@gmx.de>
2023-10-24 15:50:06 +0200

    Tuple equals überschrieben

--- src/main/java/org/example/Tuple.java
+++ src/main/java/org/example/Tuple.java
@@ -3,6 +3,21 @@ package org.example;
 public class Tuple {
 
     protected double x,y,z,w;
+    double e = 0.00001;
+    @Override
+    public boolean equals(Object obj)
+    {
+
+        if(obj instanceof Tuple other)
+        {
+            return ((Math.abs(this.x - other.x) < e) &&
+                    (Math.abs(this.y - other.y) < e) &&
+                    (Math.abs(this.z - other.z) < e) &&
+                    (Math.abs(this.w - other.w) < e));
+        }
+        return false;
+
+    }
 
 
     public Tuple(double x, double y, double z, double w) {
